d23ebc76732d48ae81ce1778adfe9be508d3105d
RajputSneha17 <sneharajput63154@gmail.com>
2024-11-02 21:33:24 +0530

    Add JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let homeservice = document.querySelector(".homeservice");
-let navButtonsBars = document.querySelector(".navButtonsBars");
+    let navButtonsBars = document.querySelector(".navButtonsBars");
 
-navButtonsBars.addEventListener("mouseover", function() {
-    homeservice.style.display = 'flex';
-});
+    navButtonsBars.addEventListener("mouseover", function() {
+        homeservice.style.display = 'flex';
+        setTimeout(() => {
+            homeservice.classList.add("active");
+        }, 100);
+    });
 
-homeservice.addEventListener("mouseout", function() {
-    homeservice.style.display = 'none';
-});
+    homeservice.addEventListener("mouseover", function() {
+        homeservice.classList.add("active");
+        homeservice.style.display = 'flex';
+    });
 
-homeservice.addEventListener("mouseover", function() {
-    homeservice.style.display = 'flex';
-});
+    homeservice.addEventListener("mouseout", function() {
+        homeservice.style.display = 'none';
+            homeservice.classList.remove("active");
+    });
